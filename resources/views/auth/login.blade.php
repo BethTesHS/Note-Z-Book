b02ef6675260d7145644118373e7824abfa7299e
BethTesHS <bethelhemtesfaye95@gmail.com>
2025-02-11 17:13:58 +0300

    Starting working on login page style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/style.css'])
+    @vite(['resources/css/sign.css'])
 
     <title>Document</title>
 
@@ -16,18 +16,17 @@
 
 {{-- @section('content') --}}
 <body class="loginBody">
-    <div class="loginBox">
-        <div class="loginWelcome">
-            <h3> Welcome Back </h3>
-        </div>
+    <card class="loginBox">
+        <h3> Welcome Back </h3>
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
-            <br>
-            <label class="loginText"> Email </label> <br>
+            {{-- <br> --}}
+
+            <label> Email </label> <br>
             <input id="email" placeholder="Please enter your email." type="email" class="loginTextArea @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autofocus> <br>
             
-            <label class="loginText"> Password </label> <br>
+            <label> Password </label> <br>
             <input id="password" placeholder="Please enter your password."type="password" class="loginTextArea @error('password') is-invalid @enderror" name="password" required> <br>
 
             
@@ -50,7 +49,7 @@
         <div>
             <a class="loginLink" href="{{ route('register') }}"> Don't have an account? Sign up!</a>
         </div>
-    </div>
+    </card>
     <script>
         window.onload = function() {
             const message = "<?php echo $message; ?>";
