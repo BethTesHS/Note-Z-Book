c33d14d4137c8a4b12c374a54707e48b660d5a7c
BethTesHS <bethelhemtesfaye95@gmail.com>
2025-02-17 15:14:09 +0300

    favicon logo changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('images/bookLogo.png') }}" type="image/png">
     @vite(['resources/css/sign.css'])
 
     <title>Document</title>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('images/bookLogo.png') }}" type="image/png">
     <title>Note-Z-Book</title>
 
     @vite(['resources/css/style.css'])
